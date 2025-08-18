916956afa26ab85e4f7a644f062fb64aa5bcd638
John Yeap <johnyeap01@gmail.com>
2025-08-18 16:35:21 +0800

    Removed logs build etc from git

--- build/tree_cylinder_lock/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/johnyeap01/pcl_ws/build/tree_cylinder_lock/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/tree_cylinder_lock/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/johnyeap01/pcl_ws/build/tree_cylinder_lock/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
