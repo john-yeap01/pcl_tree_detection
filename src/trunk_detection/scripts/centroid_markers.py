4e790cf235f992af61e0d7b4c892c4d869b5cc83
JOHN YEAP <johnyeap01@gmail.com>
2025-10-27 16:40:30 +0800

    Publish cylinder centroids in Rviz

--- src/trunk_detection/scripts/centroid_markers.py
+++ src/trunk_detection/scripts/centroid_markers.py
@@ -11,11 +11,12 @@ import math
 class ClusterCentroidMarkers:
     def __init__ (self):
         rospy.init_node("cluster_centroid_markers", anonymous=True)
-        self.sub_poses = rospy.Subscriber("/cluster_poses", PoseArray, self.pose_cb)
-        self.sub_coef  = rospy.Subscriber("/cylinder_coeffs", ModelCoefficients, self.coeff_cb)  # <<< NEW
+        self.sub_poses = rospy.Subscriber("/cluster_poses", PoseArray, self.cluster_cb)
+        self.sub_coef  = rospy.Subscriber("/cylinder_coeffs", ModelCoefficients, self.coeff_cb)  
+        self.sub_cyl_centroid = rospy.Subscriber("/cylinder_centroids", PoseArray, self.centroid_cb)
         self.pub_markers = rospy.Publisher("/cluster_markers", MarkerArray, queue_size=1)
 
-    def pose_cb(self, msg):
+    def cluster_cb(self, msg):
         m = Marker()
         m.header = msg.header
         m.ns = "cluster_centroids"
@@ -32,7 +33,7 @@ class ClusterCentroidMarkers:
             m.points.append(p.position)
             rospy.loginfo("centroid at (%.2f, %.2f, %.2f)", p.position.x, p.position.y, p.position.z)
         arr = MarkerArray(); arr.markers.append(m)
-        self.pub_markers.publish(arr)
+        # self.pub_markers.publish(arr)
 
     def coeff_cb(self, mc):
         vals = mc.values
@@ -54,15 +55,43 @@ class ClusterCentroidMarkers:
         m.pose.orientation.w = 1.0  # identity; axis = Z
         m.scale.x = 2.0 * r
         m.scale.y = 2.0 * r
-        m.scale.z = 1.0             # minimal; replace with true height later
+        m.scale.z = 100.0             # minimal; replace with true height later
         m.color.r = 0.2
         m.color.g = 0.8
         m.color.b = 0.3
         m.color.a = 0.8
 
         arr = MarkerArray(); arr.markers.append(m)
-        self.pub_markers.publish(arr)
+        # self.pub_markers.publish(arr)
         rospy.loginfo("cylinder: center=(%.2f,%.2f,%.2f) r=%.3f", px,py,pz,r)
+
+
+    def centroid_cb(self, msg):
+        if not msg.poses:
+            return
+        p = msg.poses[0].position
+
+        m = Marker()
+        m.header = msg.header
+        m.ns = "cylinder_centroid"
+        m.id = 0
+        m.type = Marker.SPHERE          # a clear dot at the centroid
+        m.action = Marker.ADD
+        m.pose.position = p
+        m.pose.orientation.w = 1.0      # identity
+        m.scale.x = 0.5                 # 15 cm ball; adjust as you like
+        m.scale.y = 0.5
+        m.scale.z = 0.5
+        m.color.r = 1.0
+        m.color.g = 0.9
+        m.color.b = 0.1
+        m.color.a = 1.0
+
+        arr = MarkerArray()
+        arr.markers.append(m)
+        self.pub_markers.publish(arr)
+
+        
 
 if __name__  == "__main__":
     ClusterCentroidMarkers()
--- src/trunk_detection/src/fit_cyl.cpp
+++ src/trunk_detection/src/fit_cyl.cpp
@@ -19,135 +19,135 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/common/centroid.h>
 
+#include <pcl_msgs/ModelCoefficients.h>   // <<< NEW
+
 using PointT = pcl::PointXYZ;
 using CloudT = pcl::PointCloud<PointT>;
 
 // normal estimation stage 
 class NeStage{
-    public:
-
-        NeStage (){
-
-        }
-
-        void apply(const CloudT::ConstPtr& in, pcl::PointCloud<pcl::Normal>::Ptr& cloud_normals){
-            pcl::NormalEstimation<PointT, pcl::Normal>::Ptr ne (new pcl::NormalEstimation<PointT, pcl::Normal>);
-            pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
-            // pcl::PointCloud<pcl::Normal>::Ptr cloud_normals;
-
-            ne->setSearchMethod (tree);
-            ne->setInputCloud (in);
-            ne->setKSearch (50);
-            ne->compute (*cloud_normals);
-        }
-
-    private:
-        
-
+public:
+  NeStage (){}
+  void apply(const CloudT::ConstPtr& in, pcl::PointCloud<pcl::Normal>::Ptr& cloud_normals){
+    pcl::NormalEstimation<PointT, pcl::Normal>::Ptr ne (new pcl::NormalEstimation<PointT, pcl::Normal>);
+    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+    ne->setSearchMethod (tree);
+    ne->setInputCloud (in);
+    // ne->setRadiusSearch(0.1f);
+    ne->setKSearch (100);
+    ne->compute (*cloud_normals);
+  }
 };
-
-
 
 // sacseg from normals
 class RansacStage{
-    public:
-        RansacStage(){
+public:
+  RansacStage(){}
+  // <<< CHANGED: add coeffs_out param
+  void apply (const CloudT::ConstPtr& in,
+              const pcl::PointCloud<pcl::Normal>::ConstPtr cloud_normals,
+              CloudT::Ptr& out,
+              pcl::ModelCoefficients::Ptr& coeffs_out) {
+    pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_CYLINDER);
+    // seg.setMethodType(pcl::SAC_MSAC);   // or pcl::SAC_RMSAC
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setNormalDistanceWeight (0.2f);
+    seg.setMaxIterations (100000);
+    // seg.setProbability(0.999f);
+    seg.setDistanceThreshold (0.1);
+    seg.setRadiusLimits (0.1, 1);
+    seg.setInputCloud (in);
+    seg.setInputNormals (cloud_normals);
 
-        }
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coeffs (new pcl::ModelCoefficients);
+    seg.segment(*inliers, *coeffs);
 
+    pcl::ExtractIndices<PointT> ex;
+    ex.setInputCloud(in);
+    ex.setIndices(inliers);
+    ex.setNegative(false);
+    if (!out) out.reset(new CloudT);
+    ex.filter(*out);
 
-        void apply (const CloudT::ConstPtr& in, const pcl::PointCloud<pcl::Normal>::ConstPtr cloud_normals, CloudT::Ptr& out) {
-            pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg;
-            seg.setOptimizeCoefficients (true);
-            seg.setModelType (pcl::SACMODEL_CYLINDER);
-            seg.setMethodType (pcl::SAC_RANSAC);
-            seg.setNormalDistanceWeight (0.1);
-            seg.setMaxIterations (10000);
-            seg.setDistanceThreshold (0.05);
-            seg.setRadiusLimits (0, 0.1);
-            seg.setInputCloud (in);
-            seg.setInputNormals (cloud_normals);
-
-            // Segment into the inlier indices
-            pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-            pcl::ModelCoefficients::Ptr coeffs (new pcl::ModelCoefficients);
-            seg.segment(*inliers, *coeffs);
-
-            // Extract the indices out 
-            pcl::ExtractIndices<PointT> ex;
-            ex.setInputCloud(in);
-            ex.setIndices(inliers);
-            ex.setNegative(false);
-            if (!out) out.reset(new CloudT);
-            ex.filter(*out);
-
-        }
-
-    private:
-
-        
-
+    // <<< NEW: hand coefficients back up
+    if (!coeffs_out) coeffs_out.reset(new pcl::ModelCoefficients);
+    *coeffs_out = *coeffs;
+  }
 };
 
 class PipelineNode{
-    public:
-        PipelineNode (ros::NodeHandle& nh, ros::NodeHandle& pnh)
-        : nh_(nh), pnh_(pnh){
-            
-            normal_est_.reset(new NeStage());
-            ransac_.reset(new RansacStage());
+public:
+  PipelineNode (ros::NodeHandle& nh, ros::NodeHandle& pnh)
+  : nh_(nh), pnh_(pnh){
+    normal_est_.reset(new NeStage());
+    ransac_.reset(new RansacStage());
 
-            sub_ = nh_.subscribe("/cluster_cloud", 1, &PipelineNode::cb, this);
-            pub_cyl_ = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_cylinder", 1);
-        }
+    sub_      = nh_.subscribe("/cluster_cloud", 1, &PipelineNode::cb, this);
+    pub_cyl_  = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_cylinder", 1);
+    pub_coef_ = nh_.advertise<pcl_msgs::ModelCoefficients>("/cylinder_coeffs", 1);  // <<< NEW
+    pub_centroid_ = nh_.advertise<geometry_msgs::PoseArray>("/cylinder_centroids", 1);
+  }
+
+private:
+  void cb(const sensor_msgs::PointCloud2ConstPtr& msg){
+    CloudT::Ptr cloud(new CloudT);
+    pcl::fromROSMsg(*msg, *cloud);
+    std::vector<int> drop; pcl::removeNaNFromPointCloud(*cloud, *cloud, drop);
+
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+    CloudT::Ptr cloud_out (new CloudT);
+    pcl::ModelCoefficients::Ptr coeffs(new pcl::ModelCoefficients);  // <<< NEW
+
+    normal_est_->apply(cloud,  cloud_normals);
+    ransac_->apply(cloud, cloud_normals, cloud_out, coeffs);         // <<< CHANGED
+
+    if (!cloud_out || cloud_out->empty()) { ROS_WARN("no cylinder inliers"); return; }
+
+    sensor_msgs::PointCloud2 out_msg;
+    pcl::toROSMsg(*cloud_out, out_msg);
+    out_msg.header = msg->header;
+    pub_cyl_.publish(out_msg);
+
+    // <<< NEW: publish cylinder coefficients for downstream marker node
+    pcl_msgs::ModelCoefficients mc;
+    mc.header = msg->header;
+    mc.values = coeffs->values;   // (px,py,pz, ax,ay,az, r)
+    pub_coef_.publish(mc);
+
+    ROS_INFO_STREAM("in=" << cloud->size() << " cyl=" << cloud_out->size());
 
 
-    private:
+    //publish the centroid of the cylinder inlier points
+    Eigen::Vector4f c;
+    pcl::compute3DCentroid(*cloud_out, c);
+    geometry_msgs::PoseArray arr;
+    arr.header = msg->header;
+    geometry_msgs::Pose P;
+    P.position.x = c[0];
+    P.position.y = c[1];
+    P.position.z = c[2];
+    P.orientation.w = 1.0;
+    arr.poses.push_back(P);
+    pub_centroid_.publish(arr);
+  }
 
-        // main node compute normals and then pass normals into ransac stage
-        void cb(const sensor_msgs::PointCloud2ConstPtr& msg){
-            // create objects 
-            CloudT::Ptr cloud(new CloudT);
-            pcl::fromROSMsg(*msg, *cloud);
-            std::vector<int> drop; pcl::removeNaNFromPointCloud(*cloud, *cloud, drop);
-
-            //Allocate output clouds for normals and output cloud 
-            pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-            CloudT::Ptr cloud_out (new CloudT);
-
-            // pass into the two processing Stages 
-            normal_est_->apply(cloud,  cloud_normals);
-            ransac_->apply(cloud, cloud_normals, cloud_out);
-
-            if (!cloud_out || cloud_out->empty()) {ROS_WARN("no cylinder inliers"); return;}
-
-
-            sensor_msgs::PointCloud2 out_msg;
-            pcl::toROSMsg(*cloud_out, out_msg);
-            out_msg.header = msg->header;
-            pub_cyl_.publish(out_msg);
-
-            // debug
-            ROS_INFO_STREAM("in=" << cloud->size() << " cyl=" << cloud_out->size());
-        }
-
-        
-        std::shared_ptr<RansacStage> ransac_;
-        std::shared_ptr<NeStage> normal_est_;
-        ros::NodeHandle nh_;
-        ros::NodeHandle pnh_;
-        ros::Subscriber sub_;
-        ros::Publisher pub_cyl_;
-
+  std::shared_ptr<RansacStage> ransac_;
+  std::shared_ptr<NeStage> normal_est_;
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber sub_;
+  ros::Publisher pub_cyl_;
+  ros::Publisher pub_coef_;   
+  ros::Publisher pub_centroid_;
 };
 
-
 int main (int argc, char** argv){
-
-    ros::init(argc, argv, "ransac_fit_node");
-    ros::NodeHandle nh, pnh;
-    PipelineNode node(nh,pnh);
-    ros::spin();
-
-    return 0;
+  ros::init(argc, argv, "ransac_fit_node");
+  ros::NodeHandle nh, pnh("~");
+  PipelineNode node(nh,pnh);
+  ros::spin();
+  return 0;
 }
